db28c1138f1384c3890622bb12a631a5eb9548aa
Steve Kay <stevekay72@gmail.com>
2023-05-29 15:15:04 +0100

    updates

--- Azure-VM-Monitor.App/Program.cs
+++ Azure-VM-Monitor.App/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Azure_VM_Monitor.App;
 using AzureVMMonitor.App.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAzVmService, AzVmService>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
